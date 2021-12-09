25e05ddd77cf800f0204fb8d25f2a9b2a15727c4
Patrick Savill <pjsavill@gmail.com>
2021-12-09 17:22:30 +1300

    Part 1 of day 5 solved

--- day5/day5.py
+++ day5/day5.py
@@ -18,10 +18,22 @@ for line in all_lines:
 print(len(point_index))
 print(max_dim)
 
-array = [[0]*max_dim]*max_dim
+array = [[0 for i in range(max_dim+1)] for j in range(max_dim+1)]
 print(len(array))
 
-for i in len(point_index):
+for i in range(len(point_index)):
     # Here we want to go through points. If horizontal, get the info
     # Then iterate over array and add them up
-    j = 1
+    if point_index[i].isNonDiagonal():
+        indexes = point_index[i].getFlatPath()
+        for i in range(int(len(indexes)/2)):
+            array[indexes[i*2]][indexes[i*2+1]] += 1
+
+total = 0
+for i in range(len(array[0])):
+    for j in range(len(array[i])):
+        if (array[i][j] >1):
+            total+= 1
+
+print("here we go")
+print(total)
--- day5/points.py
+++ day5/points.py
@@ -1,11 +1,13 @@
 class Points:
-    point1 = [0,0]
-    point2 = [-1,-1]
+    point1 = []
+    point2 = []
     def __init__(self, coord1, coord2):
-        self.point1[0] = int(coord1.split(",")[0])
-        self.point1[1] = int(coord1.split(",")[1])
-        self.point2[0] = int(coord2.split(",")[0])
-        self.point2[1] = int(coord2.split(",")[1])
+        self.point1 = []
+        self.point2 = []
+        self.point1.append(int(coord1.split(",")[0]))
+        self.point1.append(int(coord1.split(",")[1]))
+        self.point2.append(int(coord2.split(",")[0]))
+        self.point2.append(int(coord2.split(",")[1]))
 
     def isNonDiagonal(self):
         if(self.point1[0] == self.point2[0] or self.point1[1] == self.point2[1]):
@@ -15,3 +17,33 @@ class Points:
 
     def getMax(self):
         return max(self.point1[0], self.point1[1], self.point2[0], self.point2[1])
+
+    def getFlatPath(self):
+        xToY = []
+        if (self.point1[0] == self.point2[0]):
+            # Going to iterate over y
+            startY = self.point2[1]
+            xToY.append(self.point1[0])
+            xToY.append(startY)
+            while startY != self.point1[1]:
+                if (self.point2[1] > self.point1[1]):
+                    startY -=1
+                else:
+                    startY += 1
+                xToY.append(self.point1[0])
+                xToY.append(startY)
+        else:
+            # Going to iterate over x
+            startX = self.point2[0]
+            xToY.append(startX)
+            xToY.append(self.point1[1])
+            while startX != self.point1[0]:
+                if (self.point2[0] > self.point1[0]):
+                    startX -= 1
+                else:
+                    startX += 1
+                xToY.append(startX)
+                xToY.append(self.point1[1])
+
+
+        return xToY
