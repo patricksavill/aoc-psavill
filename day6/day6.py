bb19f8b9158b19e114a854edcbac54ec6dd35cff
patricksavill <pjsavill@gmail.com>
2021-12-09 21:22:22 +1300

    Working part 1 and part 2 day 6

--- day6/day6.py
+++ day6/day6.py
@@ -4,13 +4,13 @@ with open("day6.txt") as file:
     for item in all_lines[0].split(","):
         fish.append(int(item))
 
-days = 80
+days = 0
 # Life span of lantern fish: 8 on new, 6 on others
-while days < 80: # Part 1 only
+while days < 80:  # Part 1 only
     new_fish = []
     for i in range(len(fish)):
         if fish[i] == 0:
-            new_fish.append(8) # Fresh lantern added
+            new_fish.append(8)  # Fresh lantern added
             fish[i] = 6
         else:
             fish[i] -= 1
@@ -24,30 +24,25 @@ life_numbers = [0] * 9
 with open("day6.txt") as file:
     all_lines = file.readlines()
     for item in all_lines[0].split(","):
-         life_numbers[int(item)] += 1
-
-life_numbers = [z for z in range(9)]
+        life_numbers[int(item)] += 1
 
 days = 0
-while days < 2:
-
+while days < 256:
     # All zero fish get new babies at the end
-
-    print(life_numbers)
     zero_fishes = life_numbers[0]
-    six_year_old = life_numbers[5]
-    life_numbers[5] = life_numbers[6] + life_numbers[0]
+
+    # Typing it out like this helped to debug
     life_numbers[0] = life_numbers[1]
     life_numbers[1] = life_numbers[2]
+    life_numbers[2] = life_numbers[3]
     life_numbers[3] = life_numbers[4]
-    life_numbers[4] = six_year_old
+    life_numbers[4] = life_numbers[5]
+    life_numbers[5] = life_numbers[6]
 
-    life_numbers[6] = life_numbers[7]
+    life_numbers[6] = life_numbers[7] + zero_fishes
     life_numbers[7] = life_numbers[8]
     life_numbers[8] = zero_fishes
 
-    print(life_numbers)
-    print(sum(life_numbers))
     days += 1
 
 print(sum(life_numbers))
