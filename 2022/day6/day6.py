d849bf61d59765957abf33c3567a32dea7f0e18d
patricksavill <pjsavill@gmail.com>
2022-12-07 08:22:41 +1300

    Day 6 part 2 2022

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -26,6 +26,20 @@ def is_unique(char_window):
 index = 0
 while (index + 4) < len(all_lines[0]):
     if is_unique(all_lines[0][index:index+4]):
-        print(index+4)
+        print("Part one's answer is %d " % (index+4))
         break
     index +=1
+
+"""
+Part two:
+Same as part one, but a window of 14 characters
+
+Note: made a window variable here, it's more readable
+"""
+index = 0
+window = 14
+while (index + window) < len(all_lines[0]):
+    if is_unique(all_lines[0][index:index + window]):
+        print("Part two's answer is %d" % (index + window))
+        break
+    index += 1
