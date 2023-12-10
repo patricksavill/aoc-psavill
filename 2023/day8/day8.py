5c4308b87d93255b72f46f6e50089a3b1e7730d2
Patrick <11937983+patricksavill@users.noreply.github.com>
2023-12-10 19:37:12 +1300

    2023 day 8 pt 2

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -39,3 +39,59 @@ while current_node != "ZZZ":
     instr_count += 1
 
 print(steps)
+
+# Part 2
+
+starting_nodes = []
+for k in map_nodes.keys():
+    if k.endswith("A"):
+        starting_nodes.append(k)
+
+steps = 0
+instr_count = 0
+
+# Really we want to find loops, then the lowest common multiple of all the loop lengths
+loops = []
+for i in range(len(starting_nodes)):
+    instr_count = 0
+    steps = 0
+    while True:
+        if instr_count == len(instrs):
+            # Loop around instructions
+            instr_count = 0
+
+        step_instr = instrs[instr_count]
+        if step_instr == "L":
+            starting_nodes[i] = map_nodes[starting_nodes[i]][0]
+        else:
+            starting_nodes[i] = map_nodes[starting_nodes[i]][1]
+
+
+        steps += 1
+        instr_count += 1
+        if starting_nodes[i].endswith("Z"):
+            loops.append(steps)
+            break
+
+# Took these two functions from stack overflow:
+# https://stackoverflow.com/a/147539
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+running_lcm = loops[0]
+for l in loops:
+    if running_lcm == l:
+        continue
+    else:
+        running_lcm = lcm(running_lcm, l)
+    print(l)
+
+print("2023 day 8 pt 2 %d", running_lcm)
