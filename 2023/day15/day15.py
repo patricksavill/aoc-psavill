88b209c421fe038e4568f4fbedc41413d33b0e00
patricksavill <11937983+patricksavill@users.noreply.github.com>
2023-12-18 09:17:24 +1300

    2023 day 15 pt 2
    Had to debug for partial matches

--- 2023/day15/day15.py
+++ 2023/day15/day15.py
@@ -31,3 +31,57 @@ for b in hash_blocks:
 
 print(c_vals)
 print("2023 day 15 pt 1: %d" % np.sum(np.array(c_vals)))
+
+# Part 2
+
+def index_in_list(c, l):
+    for i in range(len(l)):
+        if c in l[i]:
+            # Double check this isn't a partial match
+            label = l[i].split(" ")[0]
+            if c == label:
+                return i
+    return -1
+
+# Initialise lens box
+lens_box = {}
+for i in range(256):
+    lens_box[i] = []
+
+for b in hash_blocks:
+    # Split on both = and - to get the label to hash on
+    label = b.split("=")[0]
+    label = label.split("-")[0]
+
+    lens = re.sub("[-=]", " ", b)
+
+    box_index = 0
+    for h in label:
+        box_index = hash_alg(h, box_index)
+
+    if len(lens_box[box_index]) == 0 and "-" not in b:
+        lens_box[box_index].append(lens)
+    else:
+        contents = lens_box[box_index]
+        existing_index = index_in_list(label,contents)
+        if existing_index >= 0:
+            if "=" in b:
+                # Replace lens
+                lens_box[box_index][existing_index] = lens
+            elif '-' in b:
+                # Remove lens
+                lens_box[box_index].remove(lens_box[box_index][existing_index])
+            else:
+                print("Hit edge case, shouldn't occur")
+        elif "=" in b:
+            lens_box[box_index].append(lens)
+print(lens_box)
+
+focus_power = 0
+for k in lens_box:
+    box_num = k +1
+    for i in range(len(lens_box[k])):
+        focus_power += box_num * int(lens_box[k][i].split(" ")[-1]) * (i+1)
+
+
+print("2023 day 15 pt 2: %d" % focus_power)
