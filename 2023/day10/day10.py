c7765b6fbd6aec79bc784dd39615055327d359b5
patricksavill <11937983+patricksavill@users.noreply.github.com>
2023-12-19 16:19:11 +1300

    2023 day 10 pt 2
    Having done day 18 this finally made sense to me as to how to find the
    interior pixels (or area contained) with picks theorem

--- 2023/day10/day10.py
+++ 2023/day10/day10.py
@@ -1,6 +1,5 @@
 import sys
 from pathlib import Path
-import re
 
 sys.path.append(str(Path(__file__).parent.parent))
 from aoc_utils import aoc_utils
@@ -80,7 +79,7 @@ def starting_pipes(c_arr, y, x):
     for n in neighbour_coords():
         if 0 <= y + n[0] <= len(c_arr) and \
                 0 <= x + n[1] <= len(c_arr[y]):
-            print(c_arr[y + n[0]][x + n[1]])
+
             neighbour_symbol = c_arr[y + n[0]][x + n[1]]
             shifts = coord_shift_symbol(neighbour_symbol)
             # Determine if this symbol points back to start pos
@@ -101,7 +100,7 @@ def return_next_coord(c_arr, y, x, a, b):
             # Back to the starting symbol, continue
             continue
         else:
-            print(c_arr[a + s[0]][b + s[1]], [a + s[0], b + s[1]])
+            # print(c_arr[a + s[0]][b + s[1]], [a + s[0], b + s[1]])
             return c_arr[a + s[0]][b + s[1]], a + s[0], b + s[1]
 
 
@@ -118,11 +117,9 @@ def testing_traversal(input_file):
     start_loc = find_start_pos(char_arr)
     print(char_arr[start_loc[0]][start_loc[1]])
 
-    # TODO determine starting pipes
     working_pipes = starting_pipes(char_arr, start_loc[0], start_loc[1])
     print(working_pipes)
 
-    # TODO deal with two working pipes
     working_pipes.remove(working_pipes[0])
     start_y = start_loc[0]
     start_x = start_loc[1]
@@ -139,7 +136,7 @@ def testing_traversal(input_file):
             print(steps)
             print(steps/2)
             return steps/2
-            return
+
         working_pipes.append([y1, x1])
         steps += 1
 
@@ -148,3 +145,62 @@ testing_traversal('day10-test.txt')
 
 half_steps = testing_traversal('day10-input.txt')
 print('2023 half loop length part 1:', half_steps)
+
+# Part 2
+# Counting number of squares within the pipes
+
+def shoelace_approach(input_file):
+    with open(input_file) as handle:
+        test_lines = handle.readlines()
+
+    char_arr = []
+    for l in test_lines:
+        l = l.rstrip('\n')
+        char_arr.append([str(x) for x in l])
+
+    start_loc = find_start_pos(char_arr)
+
+    working_pipes = starting_pipes(char_arr, start_loc[0], start_loc[1])
+
+    # Only use one pipe
+    working_pipes.remove(working_pipes[0])
+    start_y = start_loc[0]
+    start_x = start_loc[1]
+
+    coords = []
+    coords.append([start_x, start_y])
+    coords.append([working_pipes[0][1], working_pipes[0][0]])
+
+    while True:
+        w = working_pipes[0]
+        working_pipes.remove(w)
+        s, y1, x1 = return_next_coord(char_arr, start_y, start_x, w[0], w[1])
+
+        # Need to handle the starting position PER PIPE as there are two running round
+        start_y = w[0]
+        start_x = w[1]
+
+        coords.append([x1, y1])
+
+        if s == 'S':
+            break
+        working_pipes.append([y1, x1])
+
+    area = 0
+    perimeter = 0
+    for i in range(len(coords) - 1):
+        x1, y1 = coords[i]
+        x2, y2 = coords[i+1]
+        area += 0.5 * ((x1 * y2) - (x2 * y1))
+        perimeter += abs((x1 - x2) + (y1 - y2))
+
+    # Have to account for the start, so wrap around
+    perimeter += abs((coords[-1][0] - coords[0][0]) + (coords[-1][1] - coords[0][1]))
+
+    area = abs(area)
+
+    picks_area = int(area - (perimeter/2) + 1)
+    return picks_area
+
+picks_area = shoelace_approach('day10-input.txt')
+print("2023 day 10 pt 2: %d" % picks_area)
