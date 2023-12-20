e34129d22a041fb8c722cdfc5d44b167e447ae6e
patricksavill <11937983+patricksavill@users.noreply.github.com>
2023-12-21 11:33:04 +1300

    2023 day 17 pt 1 completed.
    This took a lot of trial and error, and reading hints to get here.
    
    Moving up to three steps in one direction, and only using right angles,
    is not something I would have thought of I don't think.

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -2,50 +2,60 @@ import sys
 from pathlib import Path
 import re
 import numpy as np
+
 sys.path.append(str(Path(__file__).parent.parent))
 from aoc_utils import aoc_utils
 
 INPUT = "day17-input.txt"
 all_lines = aoc_utils.strip_newlines(aoc_utils.file_reader(INPUT))
 
+from enum import Enum
+
+
+class Direction(Enum):
+    Up = 1
+    Right = 2
+    Down = 3
+    Left = 4
+
 
 # BFS approach
 class Grid:
-    def __init__(self, pos_x, pos_y, cost, g_x, g_y, history=[]):
+    def __init__(self, pos_x, pos_y, cost, dir, history=[]):
         # Running total of visits up to this node
         self.x = pos_x
         self.y = pos_y
-        self.goal_x = g_x
-        self.goal_y = g_y
         self.cost = cost
-        if len(history)== 0:
+        self.direction = dir
+
+        if len(history) == 0:
             self.history = [[pos_x, pos_y]]
         else:
             self.history = []
             self.history.extend(history)
             self.history.append([pos_x, pos_y])
 
-    def moved_three_in_a_row(self, new_coord):
-        if len(self.history) < 4:
-            return False
-        i = len(self.history)-4
-        x1, y1 = self.history[i]
-        x2, y2 = self.history[i+1]
-        x3, y3 = self.history[i+2]
-        x4, y4 = self.history[i + 3]
-
-        # If x or y has been the same four times in order, we moved in order and cannot go
-        # to the new coord if it matches. Note four times = 3 movements made
-        if x1 == x2 == x3 == x4 == new_coord[0]:
-            return True
-        elif y1 == y2 == y3 == y4 == new_coord[1]:
-            return True
-        return False
-
+    def parallel(self, new_dir):
+        # Up = 1, Down = 3. So difference is two
+        # Right = 2, Left = 4, so difference is still two
+        return abs(self.direction.value - new_dir.value) == 2 or self.direction == new_dir
 
     def __lt__(self, other):
         return self.cost < other.cost
 
+
+def test_parallel():
+    g = Grid(0, 0, 0, Direction.Right)
+    assert g.parallel(Direction.Left)
+    g.direction = Direction.Left
+    assert g.parallel(Direction.Right)
+    g.direction = Direction.Up
+    assert g.parallel(Direction.Down)
+    g.direction = Direction.Down
+    assert g.parallel(Direction.Up)
+
+
+test_parallel()
 
 char_arr = []
 for l in all_lines:
@@ -54,68 +64,66 @@ for l in all_lines:
 
 grid = np.array(char_arr)
 
-start_pos = [0,0]
-end_pos = [grid.shape[0]-1, grid.shape[1]-1]
+start_pos = [0, 0]
+end_pos = [grid.shape[0] - 1, grid.shape[1] - 1]
 
 import queue
+
 import imageio
+
 Q = queue.PriorityQueue()
-Q.put(Grid(start_pos[0], start_pos[1], grid[start_pos[0], start_pos[0]], end_pos[0], end_pos[1]))
-N = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Down))
+Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Right))
+
+N = [(0, 1, Direction.Down), (0, -1, Direction.Up),
+     (1, 0, Direction.Right), (-1, 0, Direction.Left)]
 
 global_costs = np.zeros((grid.shape))
-visited_set = set() # Global visiting set, preventing us going to visited locations
+visited_set = set()  # Global visiting set, preventing us going to visited locations
 
 while not Q.empty():
     curr = Q.get()
-    print(Q.qsize())
+
     # Test for success
     if curr.x == end_pos[0] and curr.y == end_pos[1]:
         print("Finished in %d" % curr.cost)
         debug_img = np.zeros((grid.shape))
-        for c in curr.history:
-            debug_img[c[0], c[1]] = 250
+        for i in range(len(curr.history)-1):
+            # Write out joined segments of history
+            debug_img[curr.history[i][0]:curr.history[i+1][0]+1, curr.history[i][1]:curr.history[i+1][1]+1] = 250
         imageio.v3.imwrite("last_curr-%d.png" % curr.cost, debug_img.astype(np.uint8))
-        # continue
         break
 
+    if (curr.x, curr.y, curr.direction) in visited_set:
+        continue
+
     # IMPORTANTLY we only add to the visited set when we pop off. I had a bug where we set visited prior to pop
-    visited_set.add((curr.x, curr.y))
-
-    for dx, dy in N:
-        new_x = curr.x + dx
-        new_y = curr.y + dy
-
-        if 0 <= new_x < grid.shape[0] and 0 <= new_y < grid.shape[1]:
-            if curr.moved_three_in_a_row([new_x, new_y]):
-                continue
-            if (new_x, new_y) in visited_set:
-                continue
-            new_cost = curr.cost + grid[new_x, new_y]
-
-            # See if next node is better than current node
-            best_cost = global_costs[new_x, new_y]
-            if new_cost < best_cost or best_cost == 0:
-                # We reach this node with a lower cost than previously achieved. Record it
-                global_costs[new_x, new_y] = new_cost
-            new_grid = Grid(new_x, new_y, new_cost, end_pos[0], end_pos[1], curr.history)
-            Q.put(new_grid)
+    visited_set.add((curr.x, curr.y, curr.direction))
 
 
+    for dx, dy, new_dir in N:
+        new_cost = curr.cost
 
-debug_img = np.zeros((grid.shape))
-for c in curr.history:
-    debug_img[c[0], c[1]] = 250
-imageio.v3.imwrite("last_curr.png", debug_img.astype(np.uint8))
+        # Here's the trick, we move up to 3 steps in one direction at a time
+        # We THEN only move next time at right angles to the previous direction, since we moved up to the max last time
+        for steps in range(1, 4):
+            new_x = curr.x + dx * steps
+            new_y = curr.y + dy * steps
+            if 0 <= new_x < grid.shape[0] and 0 <= new_y < grid.shape[1]:
+                if curr.parallel(new_dir):
+                    # Ensure we're moving at a right angle to the previous direction
+                    continue
+                # Add to the running cost of the steps in this direction
+                new_cost += grid[new_x, new_y]
 
-debug_img = np.zeros((grid.shape))
-for c in visited_set:
-    debug_img[c[0], c[1]] = 250
-imageio.v3.imwrite("visited.png", debug_img.astype(np.uint8))
+                # Debug visuals only, store best code per node
+                if new_cost < global_costs[new_x, new_y] or global_costs[new_x, new_y] == 0:
+                    global_costs[new_x, new_y] = new_cost
 
-debug_img = np.zeros((grid.shape))
+                new_grid = Grid(new_x, new_y, new_cost, new_dir, curr.history)
+                Q.put(new_grid)
+
+
 
 imageio.v3.imwrite("costs.png", global_costs.astype(np.uint8))
 
-
-print("finished")
