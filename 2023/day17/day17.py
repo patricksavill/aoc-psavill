191d3f337bd071bbe61b8cee63e38d8a79dc0eb4
patricksavill <11937983+patricksavill@users.noreply.github.com>
2023-12-21 11:40:41 +1300

    2023 day 17 pt 2. Adding in the step ranges

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -71,59 +71,70 @@ import queue
 
 import imageio
 
-Q = queue.PriorityQueue()
-Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Down))
-Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Right))
+def move_crucible(min_moves, max_moves, part_two = False):
 
-N = [(0, 1, Direction.Down), (0, -1, Direction.Up),
-     (1, 0, Direction.Right), (-1, 0, Direction.Left)]
+    Q = queue.PriorityQueue()
+    Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Down))
+    Q.put(Grid(start_pos[0], start_pos[1], 0, Direction.Right))
 
-global_costs = np.zeros((grid.shape))
-visited_set = set()  # Global visiting set, preventing us going to visited locations
+    N = [(0, 1, Direction.Down), (0, -1, Direction.Up),
+         (1, 0, Direction.Right), (-1, 0, Direction.Left)]
 
-while not Q.empty():
-    curr = Q.get()
+    global_costs = np.zeros((grid.shape))
+    visited_set = set()  # Global visiting set, preventing us going to visited locations
 
-    # Test for success
-    if curr.x == end_pos[0] and curr.y == end_pos[1]:
-        print("Finished in %d" % curr.cost)
-        debug_img = np.zeros((grid.shape))
-        for i in range(len(curr.history)-1):
-            # Write out joined segments of history
-            debug_img[curr.history[i][0]:curr.history[i+1][0]+1, curr.history[i][1]:curr.history[i+1][1]+1] = 250
-        imageio.v3.imwrite("last_curr-%d.png" % curr.cost, debug_img.astype(np.uint8))
-        break
+    while not Q.empty():
+        curr = Q.get()
 
-    if (curr.x, curr.y, curr.direction) in visited_set:
-        continue
+        # Test for success
+        if curr.x == end_pos[0] and curr.y == end_pos[1]:
+            print("Finished in %d" % curr.cost)
+            debug_img = np.zeros((grid.shape))
+            for i in range(len(curr.history)-1):
+                # Write out joined segments of history
+                debug_img[curr.history[i][0]:curr.history[i+1][0]+1, curr.history[i][1]:curr.history[i+1][1]+1] = 250
+            imageio.v3.imwrite("last_curr-%d.png" % curr.cost, debug_img.astype(np.uint8))
+            break
 
-    # IMPORTANTLY we only add to the visited set when we pop off. I had a bug where we set visited prior to pop
-    visited_set.add((curr.x, curr.y, curr.direction))
+        if (curr.x, curr.y, curr.direction) in visited_set:
+            continue
 
-
-    for dx, dy, new_dir in N:
-        new_cost = curr.cost
-
-        # Here's the trick, we move up to 3 steps in one direction at a time
-        # We THEN only move next time at right angles to the previous direction, since we moved up to the max last time
-        for steps in range(1, 4):
-            new_x = curr.x + dx * steps
-            new_y = curr.y + dy * steps
-            if 0 <= new_x < grid.shape[0] and 0 <= new_y < grid.shape[1]:
-                if curr.parallel(new_dir):
-                    # Ensure we're moving at a right angle to the previous direction
-                    continue
-                # Add to the running cost of the steps in this direction
-                new_cost += grid[new_x, new_y]
-
-                # Debug visuals only, store best code per node
-                if new_cost < global_costs[new_x, new_y] or global_costs[new_x, new_y] == 0:
-                    global_costs[new_x, new_y] = new_cost
-
-                new_grid = Grid(new_x, new_y, new_cost, new_dir, curr.history)
-                Q.put(new_grid)
+        # IMPORTANTLY we only add to the visited set when we pop off. I had a bug where we set visited prior to pop
+        visited_set.add((curr.x, curr.y, curr.direction))
 
 
+        for dx, dy, new_dir in N:
+            new_cost = curr.cost
 
-imageio.v3.imwrite("costs.png", global_costs.astype(np.uint8))
+            # Here's the trick, we move up to max_moves  in one direction at a time
+            # We THEN only move next time at right angles to the previous direction, since we moved up to the max last time
+            for steps in range(1, max_moves +1):
+                new_x = curr.x + dx * steps
+                new_y = curr.y + dy * steps
+                if 0 <= new_x < grid.shape[0] and 0 <= new_y < grid.shape[1]:
+                    # Add to the running cost of the steps in this direction
+                    new_cost += grid[new_x, new_y]
+                    if steps < min_moves:
+                        # Part two, HAVE to move at least four moves
+                        continue
 
+                    if curr.parallel(new_dir):
+                        # Ensure we're moving at a right angle to the previous direction
+                        continue
+
+                    # Debug visuals only, store best code per node
+                    if new_cost < global_costs[new_x, new_y] or global_costs[new_x, new_y] == 0:
+                        global_costs[new_x, new_y] = new_cost
+
+                    new_grid = Grid(new_x, new_y, new_cost, new_dir, curr.history)
+                    Q.put(new_grid)
+
+
+    if part_two:
+        imageio.v3.imwrite("costs-part-two.png", (global_costs * 255/global_costs.max()).astype(np.uint8))
+    else:
+        imageio.v3.imwrite("costs-part-one.png", (global_costs * 255/global_costs.max()).astype(np.uint8))
+    return curr.cost
+
+print("2023 day 17 pt 1: %d" % move_crucible(1,3))
+print("2023 day 17 pt 2: %d" % move_crucible(4,10, part_two=True))
